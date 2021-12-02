439857c78e2a6a77dcc5289c783c3df675ab92a1
darryl1975 <darryl1975@hotmail.com>
2021-12-02 16:40:59 +0800

    update to add azure logging in program.cs

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -8,6 +8,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+using Microsoft.Extensions.Logging.AzureAppServices;
+
 namespace WebApi
 {
     public class Program
@@ -17,8 +19,15 @@ namespace WebApi
             CreateHostBuilder(args).Build().Run();
         }
 
+        // https://docs.microsoft.com/en-us/azure/app-service/configure-language-dotnetcore?pivots=platform-linux
+        // https://docs.microsoft.com/en-us/aspnet/core/fundamentals/logging/?view=aspnetcore-6.0#azure-app-service
+
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureLogging(logging =>
+                {
+                    logging.AddAzureWebAppDiagnostics();
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
